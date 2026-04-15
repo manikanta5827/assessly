e6f5985bff1bf69886abc32a6a6731123a932dc6
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-04-15 17:00:22 +0530

    feat: remove ai_usage_detection column from assessments table

---
+++ backend/prisma/migrations/20260415113007_remove_ai_usage/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ai_usage_detection` on the `assessments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "assessments" DROP COLUMN "ai_usage_detection";
