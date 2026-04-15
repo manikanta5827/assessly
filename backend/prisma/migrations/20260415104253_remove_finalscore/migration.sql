30bd80e4962fcd2c752f106f038709b91d71d59e
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-04-15 16:13:26 +0530

    chore: remove final_score column from assessments table

---
+++ backend/prisma/migrations/20260415104253_remove_finalscore/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `final_score` on the `assessments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "assessments" DROP COLUMN "final_score";
